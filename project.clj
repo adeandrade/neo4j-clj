765d6fff31c7507fba7c8539b10275e7a5466359
Simon Cusack <simon.cusack@gmail.com>
2012-01-27 10:06:36 +1100

    Update to Neo4j 1.6 release.

--- project.clj
+++ project.clj
@@ -1,10 +1,10 @@
-(defproject neo4j-clj "0.0.1-SNAPSHOT"
+(defproject neo4j-clj "0.0.2-SNAPSHOT"
   :description      "Simple Clojuresque wrapper for Neo4J."
   :dependencies     [[org.clojure/clojure "1.3.0"]
-                     [org.neo4j/neo4j "1.6.M02"]]
+                     [org.neo4j/neo4j "1.6"]]
   :dev-dependencies [[commons-io/commons-io "2.0.1"]
                      [swank-clojure "1.4.0-SNAPSHOT"]
                      ;;
-                     [org.neo4j/neo4j-rest-graphdb "1.6.M02"]
+                     [org.neo4j/neo4j-rest-graphdb "1.6"]
                      [clj-http "0.2.7"]]
-  :repositories     {"neo4j" "http://m2.neo4j.org/"})
+  :repositories     {"neo4j-releases" "http://m2.neo4j.org/content/repositories/releases/"})
